b3f22f0df74accfaa839a160349ea6ccb19b336b
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-06-24 09:45:01 +0800

    save tmp wroks

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,14 +1,10 @@
-import { defineComponent, ref } from "vue";
+import { defineComponent } from "vue";
 
 export const Login = defineComponent({
   setup() {
-    const counter = ref(0);
-
-    // 返回渲染函数
-    // jsx 语法下使用 {}, 且不能在使用 sfc 下的自动拆箱取值
     return () => (
       <section style="background-color: blue; width: 100%; height: 600px">
-
+        123
       </section>
     );
   },
