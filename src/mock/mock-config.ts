b9c57cad095189e7827e85749dad00e369c7fca8
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-07-02 16:50:23 +0800

    配置通知信息

--- src/mock/mock-config.ts
+++ src/mock/mock-config.ts
@@ -1,8 +1,16 @@
 import MockJs from "mockjs";
 
 import RailwayLines from "./railway-lines.json";
+import Announcements from "./announcement.json";
 
+// 模拟线路信息
 MockJs.mock("/mock/railway-lines", {
   code: 200,
   data: RailwayLines,
+});
+
+// 模拟公告信息
+MockJs.mock(`/mock/announcements`, {
+  code: 200,
+  data: Announcements,
 });
