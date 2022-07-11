b99f154f490b897c8700246588d8400f689456f6
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-07-11 22:34:01 +0800

    添加数据源

--- src/network/apis/apis.ts
+++ src/network/apis/apis.ts
@@ -1,5 +1,6 @@
 import { AxiosResponse } from "axios";
 import { mockApi } from "../mockInit";
+import { apiManager } from "@/network/network";
 
 /**
  * @description 取得线路信息
@@ -15,4 +16,14 @@ export async function requestRailwayLinesData(): Promise<
  */
 export async function requestAnnouncements(): Promise<AxiosResponse<any, any>> {
   return mockApi.get(`/announcements`);
+}
+
+/**
+ * @brief 测试/学习接口, 切勿商用或者滥用
+ * @description 获取疫情分布相关的数据
+ */
+export async function getMapData(): Promise<AxiosResponse<any, any>> {
+  return apiManager.post(
+    "https://api.inews.qq.com/newsqa/v1/query/inner/publish/modules/list?modules=statisGradeCityDetail,diseaseh5Shelf"
+  );
 }
