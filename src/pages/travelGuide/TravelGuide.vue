031ce7ffc0ce3130338af445092f49fd2ff5f511
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-07-12 15:18:20 +0800

    set basic css

--- src/pages/travelGuide/TravelGuide.vue
+++ src/pages/travelGuide/TravelGuide.vue
@@ -18,18 +18,25 @@ onMounted(() => {});
 .box {
   width: 100%;
   height: 630px;
+
   display: grid;
+  grid-template-columns: 20% 60% 20%;
+  grid-template-rows: 1fr;
+  overflow: hidden;
 
   background: scroll center no-repeat url("/bgc.png");
   background-size: cover;
 
   &-left {
+    backdrop-filter: blur(12px);
   }
 
   &-center {
+    box-shadow: 0 0 3px black;
   }
 
   &-right {
+    backdrop-filter: blur(12px);
   }
 }
 </style>
