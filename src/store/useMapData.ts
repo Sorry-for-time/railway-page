d176fc3a934e989cee4dd6877ec568823b618774
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2022-07-13 14:19:59 +0800

    set basic store

--- src/store/useMapData.ts
+++ src/store/useMapData.ts
@@ -1,14 +1,30 @@
 import { defineStore } from "pinia";
+import { requestMapData } from "@/network/apis/apis";
+import type { RootObject } from "./receivedMapDataTypes";
+
+type OptionMapDataProperties = Partial<RootObject>;
 
 /**
  * @description 获取地图数据
  */
 export const useMapData = defineStore("mapData", {
-  state() {
-    return {};
+  state: () => {
+    return {
+      list: <OptionMapDataProperties>{},
+    };
   },
 
-  getters: {},
+  getters: {
+    simplifyMapData(state) {
+      return state.list.data?.diseaseh5Shelf.areaTree[0].children;
+    },
+  },
 
-  actions: {},
+  actions: {
+    async initialList(): Promise<void> {
+      const result = (await requestMapData()).data;
+      console.log(result);
+      this.list = result;
+    },
+  },
 });
