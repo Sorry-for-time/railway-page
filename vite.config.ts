090f569a0d5b26222a04a796cc799e7b7f280043
Shalling <65987758+Sorry-for-time@users.noreply.github.com>
2023-04-08 11:06:30 +0800

    调整开发配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { fileURLToPath, URL } from "url";
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from "@vitejs/plugin-vue-jsx";
+import autoprefixer from "autoprefixer";
 import AutoImport from "unplugin-auto-import/vite";
-import Components from "unplugin-vue-components/vite";
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers";
+import Components from "unplugin-vue-components/vite";
+import { fileURLToPath, URL } from "url";
+import { defineConfig } from "vite";
 
 /**
  * @description 返回项目下对应文件夹路径绝对值
@@ -16,31 +17,41 @@ function getPath(directoryName: string): string {
 }
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-    // element-ui 按需引入配置
-    AutoImport({
-      resolvers: [ElementPlusResolver()],
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()],
-    }),
-  ],
+export default defineConfig(() => {
+  return {
+    plugins: [
+      vue(),
+      vueJsx(),
+      // element-ui 按需引入配置
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
 
-  resolve: {
-    // 设置路径别名
-    alias: {
-      "@": getPath("./src"),
-      "@node": getPath("./node_modules"),
-      "@scss": getPath("./scss"),
+    resolve: {
+      // 设置路径别名
+      alias: {
+        "@": getPath("./src"),
+        "@node": getPath("./node_modules"),
+        "@scss": getPath("./scss"),
+      },
     },
-  },
-  server: {
-    port: 3000,
-  },
-  build: {
-    minify: "esbuild",
-  },
+
+    server: {
+      port: 3000,
+    },
+
+    build: {
+      minify: "esbuild",
+    },
+
+    css: {
+      postcss: {
+        plugins: [autoprefixer()],
+      },
+    },
+  };
 });
